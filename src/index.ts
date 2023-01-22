ab83489daf7f0d4cda52ac6aff4167b2cb1b9b6d
Rey <andrej.kolic@gmail.com>
2023-01-22 21:24:26 +0100

    webpack start script

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ function sayHi(name: string): string {
   return `Hi, ${name}!`;
 }
 
-const s: string = 2;
+// const s: string = 2;
 const externalLib: typeof ExternalLibExample = { version: 1 };
 console.log(externalLib);
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
-// import { resolve as _resolve } from 'path';
-// import * as path from 'path';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.ts',
@@ -19,5 +18,23 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'ReactTs starter',
+    }),
+  ],
+  devtool: 'eval-cheap-module-source-map',
+  devServer: {
+    // contentBase: targetAppDirectory,
+    // host: 'localhost', // required to ensure a secure origin for Auth0 usage: https://github.com/auth0/auth0-spa-js/blob/master/FAQ.md#why-do-i-get-auth0-spa-js-must-run-on-a-secure-origin
+    // port: Number.parseInt(process.env.DEVELOPMENT_SERVER_PORT || '10000', 10),
+    // historyApiFallback: true,
+    // compress: true,
+    hot: true,
+    open: true,
+    client: {
+      overlay: false,
+    },
   },
 };
