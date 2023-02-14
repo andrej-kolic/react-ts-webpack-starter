ae6a66743522b1be37ab88eff3b3b4878ba5b9fc
Rey <andrej.kolic@gmail.com>
2023-02-14 09:48:20 +0100

    add query client to test context

--- src/components/App/Content/__test__/Content.spec.tsx
+++ src/components/App/Content/__test__/Content.spec.tsx
@@ -1,17 +1,18 @@
-import { screen, render } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { renderWithNetwork } from '~/../test/unit+integration/custom';
 
 import { Content } from '..';
 
 describe('Content', () => {
   it('should render navigation bar', () => {
-    render(<Content />);
+    renderWithNetwork(<Content />);
     expect(screen.getByRole('navigation')).toBeInTheDocument();
   });
 
   it('should navigate to pages', async () => {
     const user = userEvent.setup();
-    render(<Content />);
+    renderWithNetwork(<Content />);
 
     await user.click(screen.getByText(/home/i));
     // expect(screen.getByText(/home page/i)).toBeInTheDocument();
--- test/unit+integration/custom.tsx
+++ test/unit+integration/custom.tsx
@@ -1,8 +1,38 @@
 import type { ReactElement, ReactNode, PropsWithChildren } from 'react';
 import { render } from '@testing-library/react';
 import type { RenderOptions, RenderResult } from '@testing-library/react';
-
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+import { queryClient } from '~/components/App';
+
+//
+
+type QueryClientConfig = ConstructorParameters<typeof QueryClient>[0];
+
+const queryClientConfig: QueryClientConfig = {
+  defaultOptions: queryClient.getDefaultOptions(),
+};
+
+/**
+ *
+ *
+ * @param ui      -
+ * @param config -
+ * @param options -
+ */
+export function renderWithNetwork(
+  ui: ReactElement,
+  config: QueryClientConfig = queryClientConfig,
+  options?: RenderOptions,
+): RenderResult {
+  const client = new QueryClient(config);
+
+  return render(
+    <QueryClientProvider client={client}>{ui}</QueryClientProvider>,
+    options,
+  );
+}
 
 export function renderWithRouter(
   ui: ReactElement,
@@ -13,7 +43,11 @@ export function renderWithRouter(
     window.history.pushState({}, '', route);
   }
 
-  return render(<BrowserRouter>{ui}</BrowserRouter>, options);
+  return renderWithNetwork(
+    <BrowserRouter>{ui}</BrowserRouter>,
+    undefined,
+    options,
+  );
 }
 
 export function renderInApp(
