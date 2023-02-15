1194dc5636543ec7e14be23ac9ad25b349029447
Rey <andrej.kolic@gmail.com>
2023-02-15 22:59:01 +0100

    update test

--- src/hooks/__test__/basic.spec.ts
+++ src/hooks/__test__/basic.spec.ts
@@ -27,4 +27,16 @@ describe('usePrevious', () => {
     rerender(2);
     expect(result.current).toEqual(1);
   });
+
+  it('should create context per call', () => {
+    const { result, rerender } = renderHook((value = 1) => {
+      return [
+        usePrevious(value),
+        usePrevious((value as number) + 100),
+      ] as number[];
+    });
+    expect(result.current).toEqual([undefined, undefined]);
+    rerender(2);
+    expect(result.current).toEqual([1, 101]);
+  });
 });
