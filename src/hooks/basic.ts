59bc0f180874dffdfca4ee799b90ade63a7e92bd
Rey <andrej.kolic@gmail.com>
2023-02-16 03:19:23 +0100

    add multiple implementations of usePrevious

--- src/hooks/basic.ts
+++ src/hooks/basic.ts
@@ -1,8 +1,71 @@
-import React from 'react';
+import { useEffect, useRef } from 'react';
+
+//
+
+function use<T>(this: ReturnType<typeof getPrevValue<T>>, value: T) {
+  if (value !== this.prevInput) {
+    this.prevValue = this.prevInput;
+  }
+  return this.prevValue;
+}
+
+function getPrevValue<T>(initialValue: T) {
+  return {
+    prevInput: initialValue,
+    prevValue: undefined as T | undefined,
+    use,
+  };
+}
 
 export function usePrevious<T>(value: T): T | undefined {
-  const previousInputRef = React.useRef<T>(value);
-  const previousValueRef = React.useRef<T>();
+  const ref = useRef<ReturnType<typeof getPrevValue<T>>>(getPrevValue(value));
+  return ref.current.use(value);
+}
+
+//
+
+class Previous<T> {
+  _previousInput: T;
+  _previousValue: T | undefined;
+
+  constructor(initialInput: T) {
+    this._previousInput = initialInput;
+  }
+
+  use(currentValue: T): T | undefined {
+    if (currentValue !== this._previousInput) {
+      this._previousValue = this._previousInput;
+      this._previousInput = currentValue;
+    }
+    return this._previousValue;
+  }
+}
+
+export function usePrevious5<T>(value: T): T | undefined {
+  const ref = useRef<Previous<T>>(new Previous(value));
+  return ref.current.use(value);
+}
+
+//
+
+export function usePrevious4<T>(value: T): T | undefined {
+  const ref = useRef<{ input: T; previous: T | undefined }>({
+    input: value,
+    previous: undefined,
+  });
+
+  if (value !== ref.current.input) {
+    ref.current = { input: value, previous: ref.current.input };
+  }
+
+  return ref.current.previous;
+}
+
+//
+
+export function usePrevious3<T>(value: T): T | undefined {
+  const previousInputRef = useRef<T>(value);
+  const previousValueRef = useRef<T>();
 
   if (value !== previousInputRef.current) {
     previousValueRef.current = previousInputRef.current;
@@ -10,4 +73,19 @@ export function usePrevious<T>(value: T): T | undefined {
   }
 
   return previousValueRef.current;
+}
+
+//
+
+export function usePrevious2<T>(value: T): T | undefined {
+  const ref = useRef<T>();
+  console.log('** value:', value);
+
+  useEffect(() => {
+    console.log('** value changed:', value);
+    ref.current = value;
+  }, [value]);
+
+  console.log('** previous:', ref.current);
+  return ref.current;
 }
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
-import { usePrevious } from '~/hooks/basic';
+import { usePrevious, usePrevious2 } from '~/hooks/basic';
 
 export const AboutPage = () => {
   const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(100);
   const prevCount = usePrevious(count);
 
   return (
@@ -12,6 +13,9 @@ export const AboutPage = () => {
         count: {count}, previousCount: {prevCount}
         <button onClick={() => setCount(count + 1)}>+</button>
         <button onClick={() => setCount(count - 1)}>-</button>
+      </div>
+      <div>
+        <button onClick={() => setCount2(count2 + 1)}>other: {count2}</button>
       </div>
     </div>
   );
